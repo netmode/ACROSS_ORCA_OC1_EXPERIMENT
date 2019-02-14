50a231724e82308c19afc5ead4bbd3d9cfb04c62
gkakkavas <46019826+gkakkavas@users.noreply.github.com>
2019-02-14 15:17:03 +0000

    Update sequential_sampling.py

--- final_gibbs_sampling/sequential_sampling.py
+++ final_gibbs_sampling/sequential_sampling.py
@@ -85,20 +85,20 @@ def roulette_wheel_sel2(population, probabilities):
     return res
 
 
-def local_conditional_numerator(T, TsU, GsU, GsA, GsN, myU, myA, myC):
-    P_s_num = np.exp( -1/T * local_energy.energy_calc(TsU, GsU, GsA, GsN,   myU, myA, myC) )
+def local_conditional_numerator(T, TsU, GsU, GsA, GsN, myU, myA, myC, rs):
+    P_s_num = np.exp( -1/T * local_energy.energy_calc(TsU, GsU, GsA, GsN,   myU, myA, myC, rs) )
     return P_s_num
 
 
-def local_conditional_denumerator(T, TsU, GsU, GsA, GsN, myA, myC): # Zs
+def local_conditional_denumerator(T, TsU, GsU, GsA, GsN, myA, myC, rs, poss_u): # Zs
     P_s_den = 0
-    for u in all_poss_u(myA):
-        P_s_den += local_conditional_numerator(T, TsU, GsU, GsA, GsN, u, myA, myC)
+    for u in poss_u:
+        P_s_den += local_conditional_numerator(T, TsU, GsU, GsA, GsN, u, myA, myC, rs)
     return P_s_den
 
 
-def local_conditional(T, TsU, GsU, GsA, GsN, myU, myA, myC):
-    return float(local_conditional_numerator(T, TsU, GsU, GsA, GsN, myU, myA, myC)) / local_conditional_denumerator(T, TsU, GsU, GsA, GsN, myA, myC)
+#def local_conditional(T, TsU, GsU, GsA, GsN, myU, myA, myC, rs):
+#    return float(local_conditional_numerator(T, TsU, GsU, GsA, GsN, myU, myA, myC, rs)) / local_conditional_denumerator(T, TsU, GsU, GsA, GsN, myA, myC, rs)
 
 
 def all_poss_u(a):
@@ -117,11 +117,12 @@ def temperature(sweep,c0):
     return T
 
 
-def draw_sample(T, TsU, GsU, GsA, GsN, myA, myC):
+def draw_sample(T, TsU, GsU, GsA, GsN, myA, myC, rs):
     prob_list = list()
     poss_u = all_poss_u(myA)
+    Zs = local_conditional_denumerator(T, TsU, GsU, GsA, GsN, myA, myC, rs, poss_u)
     for u in poss_u:
-        P_s = local_conditional(T, TsU, GsU, GsA, GsN, u, myA, myC)
+        P_s = local_conditional_numerator(T, TsU, GsU, GsA, GsN, u, myA, myC, rs) / Zs
         prob_list.append(P_s)
     
     ## implementing roulette (c)
@@ -129,7 +130,7 @@ def draw_sample(T, TsU, GsU, GsA, GsN, myA, myC):
     return samp
 
 
-def gibbs_samp(ID, ips, sweeps, TsU, GsU, GsA, GsN, myA, myC, myU):
+def gibbs_samp(ID, ips, sweeps, TsU, GsU, GsA, GsN, myA, myC, myU, rs):
 
     for w in range(1,sweeps+1):
         
@@ -159,7 +160,7 @@ def gibbs_samp(ID, ips, sweeps, TsU, GsU, GsA, GsN, myA, myC, myU):
                         GsU[index] = [ancestor_id] + n_vectors[3] # replace new value
         else:
             time.sleep(1.5)
-        u_star = draw_sample(T, TsU, GsU, GsA, GsN, myA, myC)
+        u_star = draw_sample(T, TsU, GsU, GsA, GsN, myA, myC, rs)
         affected = [i[0] for i in TsU]
         length = 6 + len(affected) + len(myU)*2
         MYstring = str(ID) + ' * ' + lst2str(affected) + '* ' + lst2str(myU) + '* ' + lst2str(u_star) + '* ' + str(length)
@@ -168,7 +169,7 @@ def gibbs_samp(ID, ips, sweeps, TsU, GsU, GsA, GsN, myA, myC, myU):
             os.remove('/root/total/final_gibbs_sampling/new_info.txt')
         f = open('/root/total/final_gibbs_sampling/new_info.txt', 'w+') ## must overwrite
         for w in winners:
-            if int(w[0]) != ID:
+            if int(w.split()[0]) != ID:
                 f.write(w)
         f.write(MYstring)
         f.write('\n\n')
